322b4e8822ace590fa25249f0bd5e98248c6bb47
Slient Mariana <slient.mariana@gmail.com>
2022-02-04 03:04:09 +0800

    test download file from URL via NIO

--- src/main/java/com/slientmariana/mariana/rest/CaribbeancomController.java
+++ src/main/java/com/slientmariana/mariana/rest/CaribbeancomController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
 @RestController
 @RequestMapping("caribbeancom")
 public class CaribbeancomController {
@@ -13,8 +19,25 @@ public class CaribbeancomController {
     @Autowired
     private CaribbeancomService caribbeancomService;
 
-    @GetMapping("/")
+    @GetMapping("/get/")
     public void getCaribbeancom(){
         caribbeancomService.CreateCaribbeancom();
+    }
+
+    @GetMapping("/test/download")
+    public void testDownload(){
+        try {
+            URL url = new URL("https://www.caribbeancom.com/moviepages/020222-001/images/l_l.jpg");
+            ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+
+            FileOutputStream fileOutputStream = new FileOutputStream("/Users/mariana/Downloads/l_l.jpg");
+            fileOutputStream.getChannel()
+                    .transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+
+            readableByteChannel.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
